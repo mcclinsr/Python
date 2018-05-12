b9d685d26b2b4f16855006f2b8b80e1ad193df0a
mcclinsr <mcclinsr@gmail.com>
2018-05-12 13:42:29 -0400

    added colors

--- Testin' shit/helloWorld.py
+++ Testin' shit/helloWorld.py
@@ -1,1 +1,6 @@
-print ('hello world')
+colors = ['red', 'yellow','green', 'blue']
+
+i = 0
+while i < len(colors):
+    print ('when I was %d, my favorite color was %s' % (i, colors[i]))
+    i += 1
